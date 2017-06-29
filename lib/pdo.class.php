7b22faa8c9effafd97661ae4ce653256b3990bbd
Oscar Azpeitia <neozilus@gmail.com>
2017-06-29 15:47:36 -0500

    PDO fixed

--- lib/pdo.class.php
+++ lib/pdo.class.php
@@ -4,40 +4,34 @@ class Database{
 	private $user = DB_USER;
 	private $pass = DB_PASS;
 	private $dbname = DB_NAME;
-	
 	private $dbh;
-    private $error;
-	
+  private $error;
 	private $stmt;
-	
+
 	public function __construct(){
-        // Set DSN
-        $dsn = 'mysql:host=' . $this->host . ';dbname=' . $this->dbname;
-        // Set options
-        $options = array(
-            PDO::ATTR_PERSISTENT    => true,
-            PDO::ATTR_ERRMODE       => PDO::ERRMODE_EXCEPTION,
-			PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'
-        );
-        // Create a new PDO instanace
-        try{
-            $this->dbh = new PDO($dsn, $this->user, $this->pass, $options);
-        }
-        // Catch any errors
-        catch(PDOException $e){
-            $this->error = $e->getMessage();
-        }
+    $dsn = 'mysql:host=' . $this->host . ';dbname=' . $this->dbname;
+    $options = array(
+      PDO::ATTR_PERSISTENT	=> true,
+      PDO::ATTR_ERRMODE	=> PDO::ERRMODE_EXCEPTION,
+			PDO::MYSQL_ATTR_INIT_COMMAND	=> 'SET NAMES utf8'
+    );
+    try{
+      $this->dbh = new PDO($dsn, $this->user, $this->pass, $options);
     }
-	
+    catch(PDOException $e){
+      $this->error = $e->getMessage();
+    }
+  }
+
 	public function query($query){
 		$this->stmt = $this->dbh->prepare($query);
 	}
- 	
+
 	public function bind($param, $value, $type = null){
  		if (is_null($type)) {
 			switch (true) {
 				case is_int($value):
-				  $type = PDO::PARAM_INT;
+					$type = PDO::PARAM_INT;
 				  break;
 				case is_bool($value):
 				  $type = PDO::PARAM_BOOL;
@@ -51,13 +45,13 @@ class Database{
 		}
 		$this->stmt->bindValue($param, $value, $type);
 	}
-	
+
 	public function bindArray($array){
 		foreach($array as $key => $value){
 		$this->bind($key, $value);
 		}
 	}
-	
+
 	public function execute(){
     	return $this->stmt->execute();
 	}
@@ -66,20 +60,20 @@ class Database{
 		$this->execute();
 		return $this->stmt->fetchAll(PDO::FETCH_ASSOC);
 	}
-	
+
 	public function single(){
 		$this->execute();
 		return $this->stmt->fetch(PDO::FETCH_ASSOC);
 	}
-	
+
 	public function rowCount(){
 		return $this->stmt->rowCount();
 	}
-	
+
 	public function lastInsertId(){
 		return $this->dbh->lastInsertId();
 	}
-	
+
 	public function beginTransaction(){
 		return $this->dbh->beginTransaction();
 	}
@@ -87,7 +81,7 @@ class Database{
 	public function endTransaction(){
 		return $this->dbh->commit();
 	}
-	
+
 	public function cancelTransaction(){
 		return $this->dbh->rollBack();
 	}
